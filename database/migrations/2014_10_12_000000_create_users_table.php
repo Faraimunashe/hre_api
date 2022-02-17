078e62970aa9046f64de4369268e2ceca0e8f576
Faraimunashe <50617281+Faraimunashe@users.noreply.github.com>
2022-02-17 04:08:14 +0200

    Update 2014_10_12_000000_create_users_table.php
    remove relationship

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreign('acc_id')->references('id')->on('accounts')->onDelete('cascade');
         });
     }
 
